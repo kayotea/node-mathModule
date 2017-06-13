3e7d1fb7c4c42f67a1357ecb6091e734a34d2f87
Placoderm <placoderm@Sarah-MacBook-Air.local>
2017-06-12 18:16:12 -0700

    initial commit

---
+++ mathlib.js
@@ -0,0 +1,21 @@
+module.exports = {
+  //return {
+    add:  function(num1, num2) { 
+         return num1+num2;
+    },
+    multiply: function(num1, num2) {
+         return num1*num2;
+    },
+    square: function(num1) {
+         return num1*num1;
+    },
+    random: function(num1, num2) {
+      if (num1 >= num2) {
+         return Math.floor(Math.random()*(num1-num2)+num2);
+      } else {
+         return Math.floor(Math.random()*(num2-num1)+num1);
+      }
+    }
+  //}
+};
+
